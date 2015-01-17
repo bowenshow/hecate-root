d161311b6b81a468e1ed529d921111f5f00b95d7
Bowen <gaobw@hesong.net>
2015-01-17 18:25:10 +0800

    nil

--- hecate-persist/src/main/java/com/chaos/hecate/persist/user/service/UserManager.java
+++ hecate-persist/src/main/java/com/chaos/hecate/persist/user/service/UserManager.java
@@ -3,13 +3,14 @@ package com.chaos.hecate.persist.user.service;
 import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.chaos.hecate.core.AbstractManager;
 import com.chaos.hecate.persist.user.dao.UserDao;
 import com.chaos.hecate.persist.user.model.User;
 import com.chaos.hecate.utils.persistence.BaseDaoInterface;
 
-
+@Component
 public class UserManager extends AbstractManager<User> {
 	
 	@Autowired
